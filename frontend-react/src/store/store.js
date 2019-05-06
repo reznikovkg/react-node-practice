6eb55869fa5eb2105e7f89fa02829bc806d9f7a0
Reznikovkg <reznikov.kosta@yandex.ru>
2019-05-06 16:08:18 +0300

    remover redux devtools

--- frontend-react/src/store/store.js
+++ frontend-react/src/store/store.js
@@ -4,7 +4,7 @@ import mainReducer from './reducers';
 import initialState from './initialState';
 
 const configureStore = (state = initialState, reducer = mainReducer) => {
-    return createStore(reducer, state, window.__REDUX_DEVTOOLS_EXTENSION__());
+    return createStore(reducer, state);
 };
 
 export default configureStore;
