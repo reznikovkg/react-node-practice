23ec953dc37b81b5f228ea545b087d205b49e852
Reznikovkg <reznikov.kosta@yandex.ru>
2019-05-13 13:18:16 +0300

    fix docker

--- backend/start.sh
+++ backend/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker exec -it react-node-practice_web_1 npx makemigration --name
-docker exec -it react-node-practice_web_1 npx runmigration
-docker exec -it react-node-practice_web_1 node_modules/.bin/sequelize db:seed:all
+npx makemigration --name
+npx runmigration
+node_modules/.bin/sequelize db:seed:all
 
 npm start
--- crash-project.sh
+++ crash-project.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 docker rm react-node-practice_db_1
-
 docker rm react-node-practice_web_1
+docker rm react-node-practice_front_1
 
+docker rmi react-node-practice_front
 docker rmi react-node-practice_web
+docker rmi react-node-practice_db
