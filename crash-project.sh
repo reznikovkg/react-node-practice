fa102f57e90a3e1c4e635ea51a36ca6aa03ca4f3
Reznikovkg <reznikov.kosta@yandex.ru>
2019-05-13 13:31:31 +0300

    fix crash

--- crash-project.sh
+++ crash-project.sh
@@ -2,8 +2,8 @@
 
 docker rm react-node-practice_db_1
 docker rm react-node-practice_web_1
-docker rm react-node-practice_front_1
+docker rm react-node-practice_frontend-react_1
 
-docker rmi react-node-practice_front
+docker rmi react-node-practice_frontend-react
 docker rmi react-node-practice_web
 docker rmi react-node-practice_db
